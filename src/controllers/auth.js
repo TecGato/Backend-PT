9e6db64649fbfec649c2961429ddf85dd899b2fd
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-05-18 20:51:00 -0400

    [Add] Auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,24 +5,26 @@ const { User } = require('../db');
 const { SECRET_KEY, SALT } = process.env;
 
 async function register(req, res) {
-  const { email, password } = req.body;
+  const { email, password, name } = req.body;
   try {
-    if (!email || !password) {
-      return res.status(400).send('Missing email or password');
+    if (!email || !password || !name) {
+      return res.status(400).send('Missing email, password or name');
     }
     const user = await User.findOne({ where: { email } });
     if (user) {
       return res.status(400).send('User already exists');
     }
-    const salt = await bcrypt.genSalt(SALT);
+    console.log(SALT);
+    const salt = await bcrypt.genSalt(parseInt(SALT));
     const hash = await bcrypt.hash(password, salt);
     const newUser = await User.create({
+      name,
       email,
       password: hash,
     });
+    res.status(201).json('User created successfully');
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
+    res.status(500).json({ error: error.message });
   }
 }
 
@@ -40,8 +42,14 @@ async function login(req, res) {
     if (!match) {
       return res.status(400).send('Invalid password');
     }
-    const token = jwt.sign({ id: user.id }, SECRET_KEY);
-    res.json({ token });
+    const payload = { email: user.email, password: user.password };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+    res.cookie('token', token, {
+      maxAge: 1000 * 60 * 60 * 1,
+      sameSite: 'none',
+      secure: true,
+    });
+    res.status(200).send("You're logged in");
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
